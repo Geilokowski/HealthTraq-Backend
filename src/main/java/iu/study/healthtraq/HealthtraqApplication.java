a05b3de8b2fbbc28c1eef85135829a60cdb3a073
Adrian Lübke <adrian.luebke@iu-study.org>
2023-06-16 14:53:58 +0200

    Bare Bones Application

---
+++ src/main/java/iu/study/healthtraq/HealthtraqApplication.java
@@ -0,0 +1,13 @@
+package iu.study.healthtraq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthtraqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthtraqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iu/study/healthtraq/HealthtraqApplicationTests.java
@@ -0,0 +1,13 @@
+package iu.study.healthtraq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthtraqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
