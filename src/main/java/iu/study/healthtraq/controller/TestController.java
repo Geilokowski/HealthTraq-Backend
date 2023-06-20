5b6dfbe8510f96b3eb4f843041f37b26f8494f05
Adrian Lübke <adrian.luebke@iu-study.org>
2023-06-20 11:42:15 +0200

    added database

--- src/main/java/iu/study/healthtraq/controller/TestController.java
+++ src/main/java/iu/study/healthtraq/controller/TestController.java
@@ -1,11 +1,10 @@
-package iu.study.healthtraq;
+package iu.study.healthtraq.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
     @GetMapping(path = "/test")
     public String getTestResponse(){
         return "hello world";
---
+++ src/main/java/iu/study/healthtraq/models/User.java
@@ -0,0 +1,18 @@
+package iu.study.healthtraq.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+}
