1b191debb0ab76bab4b27043ef31cc123f4bb917
study-ekrause <erik.krause@iu-study.org>
2023-07-03 17:18:23 +0200

    Klassen nach Klassendiagramm angelegt
    Habe erstmal die Klassen angelegt, wie wir sie im Klassendiagramm bestimmt hatten, Absprachen noch nötig wegen finalen Variablen in den Klassen.

---
+++ src/main/java/iu/study/healthtraq/models/HeartRate.java
@@ -0,0 +1,10 @@
+package iu.study.healthtraq.models;
+
+import java.util.Date;
+
+public class HeartRate {
+
+    float averageHeartRate;
+    float maximumHeartRate;
+    Date time;
+}
---
+++ src/main/java/iu/study/healthtraq/models/WatchUserPolar.java
@@ -0,0 +1,19 @@
+package iu.study.healthtraq.models;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class WatchUserPolar {
+
+    int polarUserId;
+    String polarMemberId;
+    Date registrationDate;
+    String firstName;
+    String lastName;
+    Date birthDate;
+    String gender;
+    int weight;
+    int height;
+
+
+}
